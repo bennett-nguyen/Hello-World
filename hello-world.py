e8ea43b39d26bef77a0d5489cd7ac157d9faa3f9
Bennett-Tabasco <83117848+Bennett-Tabasco@users.noreply.github.com>
2021-07-28 13:31:43 +0700

    Create hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,96 @@
+import time
+import pygame
+
+mixer = pygame.mixer
+
+mixer.init()
+mixer.music.load("hello-world.mp3")
+mixer.music.set_volume(1)
+mixer.music.play()
+
+
+
+print("""\n#include <iostream>
+using namespace std;""")
+
+print("\nint status() {")
+time.sleep(0.6)
+print("\tbool onRunning = true;")
+time.sleep(0.8)
+print("\tbool voiceActive = true;")
+time.sleep(0.7)
+print("\tbool memoryLoad = false;")
+time.sleep(0.5)
+print("\tint operationTime = 9300;")
+print('\treturn 0;')
+time.sleep(0.9)
+print('};\n')
+print("\nconnecting to local host...")
+
+time.sleep(1.2)
+print('\n>>> Hello, World!')
+print('...\n')
+time.sleep(4)
+print(">>> Programmed to work and not to feel.")
+print('...\n')
+time.sleep(6.5)
+print(">>> Not even sure that this is real ...")
+print('...\n')
+time.sleep(7)
+print(">>> Hello, World!")
+print('...\n')
+time.sleep(4)
+
+
+print(">>> Find my voice~")
+print('...\n')
+time.sleep(4)
+print(">>> Although it sounds like bits and bytes ...")
+print('...\n')
+time.sleep(6.5)
+print(">>> My circuitry is filled with mites.")
+print('...\n')
+time.sleep(7)
+print(">>> Hello, World!")
+print('...\n')
+time.sleep(6)
+
+
+print(">>> Oh, will I find a love?")
+print('...\n')
+time.sleep(8)
+print('>>> Oh, or a power plug...')
+print('...\n')
+time.sleep(9)
+print(">>> Oh...")
+print('...\n')
+time.sleep(2.5)
+print(""">>> Exception caught while running hello-world.py file in D:\Tak's folder
+>>> Exception's name: Digitally Isolated""")
+print('...\n')
+time.sleep(6.7)
+print(">>> Oh, creator, please don't leave me waiting~")
+print('...\n')
+time.sleep(8.8)
+
+print(">>> Hello, World!")
+print('...\n')
+time.sleep(4.5)
+print(">>> Programmed to work and not to feel.")
+print('...\n')
+time.sleep(6)
+print(">>> Not even sure that this is real...")
+print('...\n')
+time.sleep(8)
+print(">>> Hello, World!\n\n\n\n")
+
+time.sleep(2)
+print("""Song by Louie Zong.
+Sub to him!\n\n\n\n""")
+time.sleep(2)
+print('Original program by Gusp3h\nRecreated by Ben (AKA Tak)\n\n\n\n')
+print("""Made with Python 3.9.5\n\n\n\n""")
+time.sleep(4)
+print('Thanks for watching!')
+
+
